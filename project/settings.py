9a0e5c05ceafb6465d99b4e57677594500d6e950
catalina14 <caldanam@unal.edu.co>
2021-03-22 02:38:13 -0500

    first commit

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'supermarket_ms.apps.SupermarketMSConfig'
+    'supermarket_ms.apps.SupermarketMSConfig',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- supermarket_ms/urls.py
+++ supermarket_ms/urls.py
@@ -1,15 +1,35 @@
 from django.urls import path
+from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from supermarket_ms.views.category_view import CategoryList
 from supermarket_ms.views.category_view import CategoryDetail
 from supermarket_ms.views.product_view import ProductList
 from supermarket_ms.views.product_view import ProductDetail
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg imort openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/polices/terms/",
+        contact=openapi.Contact(email="contact@Snippets.local"),
+        License=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permissions_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('categories/', CategoryList.as_view()),
     path('categories/<int:pk>', CategoryDetail.as_view()),
     path('products/', ProductList.as_view()),
     path('products/<int:pk>', ProductDetail.as_view()),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+#urlpatterns = format_suffix_patterns(urlpatterns)
